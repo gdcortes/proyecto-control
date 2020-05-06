714d2b9e41a6fef78d9d77b9103417ce14967b08
Gonzalo Cortes <gdcortes@uc.cl>
2020-05-06 02:34:27 -0400

    flecha incluida e intento de roce

--- grua.m
+++ grua.m
@@ -1,16 +1,19 @@
 function yprima = grua(t,y) %solo brazo en elevacion, sin roce ni torque
 
-Lb=2; % largo brazo
-Mb=1; % masa brazo
+Lb=5; % largo brazo
+Mb=1000; % masa brazo
 g=9.81;
+
+Lf=2;
+Mf=1000;
 
 
 yprima(1)=y(2); %velocidad en elevacion (alfa)
-yprima(2)=((1/2)*(y(4)^2)*(sin(2*y(1)))*((1/3)*Mb*Lb^2)+g*(sin(y(1)))*(Mb*(Lb/2)))/((1/3)*Mb*Lb^2);
+yprima(2)= -0.7*(y(2))    +((1/2)*(y(4))^2*sin(2*y(1))*((1/3)*Mb*Lb^2+(1/12)*Mf*Lf^2+Mf*(Lb-y(5)+(Lf/2))^2) + g*sin(y(1))*(Mb*Lb/2+Mf*(Lb-y(5)+(Lf/2))) -y(2)*(-2*Mf*(Lb-y(5)+(Lf/2))*y(6))    )/ ((1/3)*Mb*Lb^2 +(1/12)*Mf*Lf^2  + Mf*(Lb-y(5)+(Lf/2))^2);
 yprima(3)=y(4);
-yprima(4)=(-(y(4)^2)*(sin(2*y(1)))*((1/3)*Mb*Lb^2))/(((sin(y(1)))^2)*(1/3)*Mb*Lb^2);
-yprima(5)=y(5);
-%yprima(6)=;
+yprima(4)=(-(y(4))*(sin(y(1))^2)*(-2*Mf*(Lb-y(5)+(Lf/2))*y(6))-y(4)*sin(2*y(1))*((1/3)*Mb*(Lb^2)+(1/12)*Mf*(Lf^2)+Mf*(Lb-y(5)+(Lf/2))^2))/((sin(y(1))^2)*((1/3)*Mb*(Lb^2)+(1/12)*Mf*(Lf^2)+Mf*(Lb-y(5)+(Lf/2))^2));
+yprima(5)=y(6);
+yprima(6)=((1/2)*((y(2)^2)+(y(4)^2)*(sin(y(1))^2))*(-2*Mf*(Lb-y(5)+(Lf/2))*y(6))-g*cos(y(1))*(-Mf*y(6)))/Mf;
 
 yprima=yprima';
 
--- grua_ode.m
+++ grua_ode.m
@@ -1,14 +1,14 @@
-% CAIDA LIBRE DEL PENDULO
+% CAIDA LIBRE GRUA
 clear
 %F=0; % fuerza externa en cero
-%rho_a=0; % coeficiente roce con el aire
-y0=[10*pi/180 0 10*pi/180 0 0]; % condiciones iniciales
-dt=0.01;  % periodo del sistema
+rho_a=1; % coeficiente roce con el aire
+y0=[10*pi/180 0 10*pi/180 0 0 0]; % condiciones iniciales
+dt=0.1;  % periodo del sistema
 k=1;
-tmax=20.0;  % tiempo maximo de la simulacion
+tmax=20;  % tiempo maximo de la simulacion (ojo con tiempo de simulacion para valores iniciales muy altos)
 
 % inicializacion variables de salida
-yt=zeros(fix(tmax/dt)+1,5);
+yt=zeros(fix(tmax/dt)+1,6);
 tt=zeros(fix(tmax/dt)+1,1);
 
 for t1=0:dt:tmax
@@ -22,8 +22,8 @@ for t1=0:dt:tmax
     if yt(k,1)<0, yt(k,1)=yt(k,1)+2*pi; end;
     if yt(k,1)>2*pi, yt(k,1)=yt(k,1)-2*pi; end;
     
-    if yt(k,3)<0, yt(k,3)=yt(k,3)+2*pi; end;
-    if yt(k,3)>2*pi, yt(k,3)=yt(k,3)-2*pi; end;
+   % if yt(k,3)<0, yt(k,3)=yt(k,3)+2*pi; end;
+    %if yt(k,3)>2*pi, yt(k,3)=yt(k,3)-2*pi; end;
     
     % toma ultimo valor tiempo simulado
     tt(k,:)=t(max(size(y)));
@@ -36,6 +36,7 @@ for t1=0:dt:tmax
 end
 
 % dibuja respuesta de angulo
-plot(tt,yt(:,1)*180/pi); xlabel('tiempo, segs'); ylabel('Alfa, grados'); pause
+plot(tt,yt(:,1)*180/pi); xlabel('tiempo, segs'); ylabel('Alfa, grados'); %pause
 
-plot(tt,yt(:,3)*180/pi); xlabel('tiempo, segs'); ylabel('Beta, grados'); %pause
+%plot(tt,yt(:,2)); xlabel('tiempo, segs'); ylabel('velocidad angular alfa');% pause
+%plot(tt,yt(:,3)*180/pi); xlabel('tiempo, segs'); ylabel('Beta, grados'); %pause
