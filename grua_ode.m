fa20bb929c1e2c01b431b2955c8ebf4a4d409f17
Gonzalo Cortes <gdcortes@uc.cl>
2020-05-09 03:38:17 -0400

    modelo definitivo

--- grua_ode.m
+++ grua_ode.m
@@ -1,11 +1,11 @@
-% CAIDA LIBRE GRUA
+% %%%%%%%%%%%%%%%%%%% GRUA LAZO ABIERTO %%%%%%%%%%%%%%%%%
 clear
-%F=0; % fuerza externa en cero
 
-y0=[(pi/3) -0.6 (pi/6) 0.6 5 1]; % condiciones iniciales
+
+y0=[ (pi/3) -1 (pi/3) 1 5 1]; % condiciones iniciales
 dt=0.1;  % periodo del sistema
 k=1;
-tmax=20;  % tiempo maximo de la simulacion (ojo con tiempo de simulacion para valores iniciales muy altos)
+tmax=8;  % tiempo maximo de la simulacion (ojo con tiempo de simulacion para valores iniciales muy altos)
 
 % inicializacion variables de salida
 yt=zeros(fix(tmax/dt)+1,6);
@@ -15,24 +15,44 @@ tt=zeros(fix(tmax/dt)+1,1);
 
 for t1=0:dt:tmax
     % integrador numerico
-    [t,y]=ode23(@(t,y) grua2(t,y),[t1 t1+dt],y0);
+    
+    if t1<2
+        t_alfa=190000;
+        t_beta=10000;
+        f_flecha=1000;
+    end
+    if t1>=2
+        t_alfa=0;
+        t_beta=10000;
+        f_flecha=1000;
+    end
+   
+    
+    [t,y]=ode23(@(t,y) grua2(t,y,t_alfa,t_beta,f_flecha),[t1 t1+dt],y0);
     
     % toma ultimo valor del vector
     yt(k,:)=y(max(size(y)),:);
     
     % usa valor entre 0 y 2pi para el beta
-    if yt(k,3)<0, yt(k,3)=yt(k,3)+2*pi; end;
-    if yt(k,3)>2*pi, yt(k,3)=yt(k,3)-2*pi; end;
+    if yt(k,3)<0, yt(k,3)=yt(k,3)+2*pi; end
+    if yt(k,3)>2*pi, yt(k,3)=yt(k,3)-2*pi; end
     
     
-    if yt(k,1)>pi/2, yt(k,1)=pi/2;end;
-    if yt(k,1)>pi/2, yt(k,2)=0;end;
+    if yt(k,1)>pi/2
+        yt(k,1)=pi/2;
+        yt(k,2)=0;
+    end
+    %if yt(k,1)>pi/2, yt(k,2)=0;end;
     
-    if yt(k,1)<0, yt(k,1)=0;end;
-    if yt(k,1)<0, yt(k,2)=0;end;
+    if yt(k,1)<0.1 
+        yt(k,1)=0.1;
+        yt(k,2)=0;
+    end
     
-    if yt(k,5)>85  %si la longitud de la flecha es mayor a 85 respecto al final del brazo (es decir la flecha mide maximo 85)
-        yt(k,5)=85;
+    %if yt(k,1)<0, yt(k,2)=0;end;
+    
+    if yt(k,5)>80  %si la longitud de la flecha es mayor a 50 respecto al final del brazo (es decir la flecha mide maximo 50)
+        yt(k,5)=80;
         yt(k,6)=0; 
     end
     
@@ -46,14 +66,6 @@ for t1=0:dt:tmax
         yt(k,6)=24;
     end
     
-    if yt(k,4)>5
-        yt(k,4)=5;
-    end
-    
-    if yt(k,4)<-5
-        yt(k,4)=-5;
-    end
-    
    
    
     % toma ultimo valor tiempo simulado
@@ -65,6 +77,9 @@ for t1=0:dt:tmax
     % incrementa periodo
     k=k+1;
 end
+
+
+
 
 figure(1)
 subplot(3,1,1)
@@ -88,3 +103,4 @@ plot(tt,yt(:,4)*180/pi); xlabel('tiempo, segs'); ylabel('Velocidad beta');
  subplot(3,1,3)
 
 plot(tt,yt(:,6)); xlabel('tiempo, segs'); ylabel('Velocidad flecha'); 
+
